200a51cb3fd17734dada1d9910678f30a39d5214
pascwin <winner@hm.edu>
2020-12-01 20:21:18 +0100

    Kompetenzen optimiert

--- js/competence.js
+++ js/competence.js
@@ -2,12 +2,11 @@ const addNewCompetence = () => {
     const dropdown = document.getElementById("selectCompetence").value;
     const container = document.getElementById("competence-container")
     const container2 = document.createElement("div");
-    container2.className="row col-xl-8"
+    container2.className = "row col-xl-8"
     const headerContainer = document.createElement("div");
-    headerContainer.className="form-group"
+    headerContainer.className = "form-group"
     const dropdownContainer = document.createElement("div");
-    dropdownContainer.className="form-group"
-    console.log(dropdown);
+    dropdownContainer.className = "form-group"
     if (dropdown === "Wählen Sie eine Kompetenz") {
         return;
     } else {
@@ -18,7 +17,6 @@ const addNewCompetence = () => {
         container2.appendChild(headerContainer);
         container2.appendChild(dropdownContainer);
         container.appendChild(container2);
-        console.log(container)
     }
 }
 
@@ -34,40 +32,37 @@ const createNewCompetenceElement = () => {
     let competenceDropdown = document.createElement("select")
     competenceDropdown.className = ("form-control col-xl-12")
     if (dropdown === "Programmieren") {
-        let newOption = option("bitte wählen")
-        let newOption1 = option("javascript");
-        let newOption2 = option("java")
-        let newOption3 = option("python")
-        let newOption4 = option("C++")
-        competenceDropdown.appendChild(newOption);
-        competenceDropdown.appendChild(newOption1);
-        competenceDropdown.appendChild(newOption2);
-        competenceDropdown.appendChild(newOption3);
-        competenceDropdown.appendChild(newOption4);
+        options = createOptions("bitte wählen", "javascript", "java", "python", "C++");
+        options.map(option => {
+            competenceDropdown.appendChild(option);
+        })
     } else if (dropdown === "Modellierung") {
-        let newOption = option("bitte wählen")
-        let newOption1 = option("Klassendiagramm");
-        let newOption2 = option("UML")
-        let newOption3 = option("Aktivitätsdiagramm")
-        let newOption4 = option("Sequenzdiagramm")
-        competenceDropdown.appendChild(newOption);
-        competenceDropdown.appendChild(newOption1);
-        competenceDropdown.appendChild(newOption2);
-        competenceDropdown.appendChild(newOption3);
-        competenceDropdown.appendChild(newOption4);
+        options = createOptions("bitte wählen", "Klassendiagramm", "UML", "Aktivitätsdiagramm", "Sequenzdiagramm");
+        options.map(option => {
+            competenceDropdown.appendChild(option);
+        })
+    } else if (dropdown === "test") {
+        options = createOptions("1", "2", "3", "4", "5");
+        options.map(option => {
+            competenceDropdown.appendChild(option);
+        })
     } else if (dropdown === "Design-Tools") {
-        let newOption = option("bitte wählen")
-        let newOption1 = option("Figma");
-        let newOption2 = option("Marvel")
-        let newOption3 = option("Adope XD")
-        let newOption4 = option("Overflow")
-        competenceDropdown.appendChild(newOption);
-        competenceDropdown.appendChild(newOption1);
-        competenceDropdown.appendChild(newOption2);
-        competenceDropdown.appendChild(newOption3);
-        competenceDropdown.appendChild(newOption4);
+        options = createOptions("bitte wählen", "Figma", "Marvel", "Adope XD", "Overflow");
+        options.map(option => {
+            competenceDropdown.appendChild(option);
+        })
     }
     return competenceDropdown;
+}
+
+createOptions = (option1, option2, option3, option4, option5) => {
+    let newOption = option(option1)
+    let newOption1 = option(option2);
+    let newOption2 = option(option3)
+    let newOption3 = option(option4)
+    let newOption4 = option(option5)
+    options = [newOption, newOption1, newOption2, newOption3, newOption4]
+    return options
 }
 
 const option = (option) => {
